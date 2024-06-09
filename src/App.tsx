b94ec5fa14ca17e67ef7e1233214439c24c73431
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-06-08 20:41:14 -0500

    added button and alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
-import ListGroup from "./components/ListGroup";
-function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+// import ListGroup from "./components/ListGroup";
+// function App() {
+//   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+//   const handleSelectItem = (item: string) => {
+//     console.log(item);
+//   };
+//   return (
+//     <div>
+//       <ListGroup
+//         items={items}
+//         heading="Cities"
+//         onSelectItem={handleSelectItem}
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// import Alert from "./components/Alert";
+// function App() {
+//   return (
+//     <div>
+//       <Alert>Hello World</Alert>
+//     </div>
+//   );
+// }
+
+// export default App;
+import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button color="secondary" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
     </div>
   );
 }
