fb38e6b778b2b536706df55c5e91ab3c4cfea537
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-06-08 19:52:22 -0500

    list group added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
-import Message from "./Message";
-function App(){
-  return <div><Message /></div>;
+import ListGroup from "./components/ListGroup";
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
